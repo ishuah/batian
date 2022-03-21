a7369c7f3693d10bf97fe295c2c0499c450677b4
Ishuah Kariuki <kariuki@ishuah.com>
2022-03-21 21:55:09 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Box, Main, Heading, Paragraph, WorldMap } from 'grommet';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet>
+      <Main align="center" pad="large">
+        <Heading>Batian</Heading>
+        <Paragraph>A happy spatial data visualization tool.</Paragraph>
+        <Box pad="large">
+          <WorldMap
+            color='status-ok'
+          />
+        </Box>
+      </Main>
+  </Grommet>
   );
 }
 
