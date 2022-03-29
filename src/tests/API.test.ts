b20866392bf75558f0a37618fe88114d9c249ec0
Ishuah Kariuki <kariuki@ishuah.com>
2022-03-29 19:49:03 +0300

    Improved coverage

--- src/tests/API.test.ts
+++ src/tests/API.test.ts
@@ -85,7 +85,7 @@ describe("getSites", () => {
     const mockRawSiteResponse = {
       "meta": {
         "limit": 20,
-        "next": "",
+        "next": "/api/v1/site/?&layer=1&limit=3&offset=3",
         "offset": 0,
         "previous": null,
         "total_count": 3
@@ -163,13 +163,98 @@ describe("getSites", () => {
       ]
     };
 
-    const mockSiteResponse = { id: 1, sites: mockRawSiteResponse["objects"] as unknown as Site[]  } as unknown as LayerObject;
+    const mockRawSiteResponseDefault = {
+      "meta": {
+        "limit": 3,
+        "next": "",
+        "offset": 3,
+        "previous": "/api/v1/site/?username=web-client&api_key=2751c89da102ccb4d042a6ddf4a0ef7296b5119d&layer=1&limit=3&offset=0",
+        "total_count": 7342
+      },
+      "objects": [
+        {
+          "data": {
+            "pop_max": 19698
+          },
+          "id": 4,
+          "name": "Canelones",
+          "resource_uri": "/api/v1/site/4/",
+          "shapes": [
+            {
+              "content_object": "/api/v1/site/4/",
+              "id": 4,
+              "object_id": 4,
+              "resource_uri": "",
+              "shape": {
+                "coordinates": [
+                  -56.2840015,
+                  -34.538004
+                ],
+                "type": "Point"
+              }
+            }
+          ]
+        },
+        {
+          "data": {
+            "pop_max": 32234
+          },
+          "id": 5,
+          "name": "Florida",
+          "resource_uri": "/api/v1/site/5/",
+          "shapes": [
+            {
+              "content_object": "/api/v1/site/5/",
+              "id": 5,
+              "object_id": 5,
+              "resource_uri": "",
+              "shape": {
+                "coordinates": [
+                  -56.2149984,
+                  -34.099002
+                ],
+                "type": "Point"
+              }
+            }
+          ]
+        },
+        {
+          "data": {
+            "pop_max": 61845
+          },
+          "id": 6,
+          "name": "Bassar",
+          "resource_uri": "/api/v1/site/6/",
+          "shapes": [
+            {
+              "content_object": "/api/v1/site/6/",
+              "id": 6,
+              "object_id": 6,
+              "resource_uri": "",
+              "shape": {
+                "coordinates": [
+                  0.7890036,
+                  9.2610001
+                ],
+                "type": "Point"
+              }
+            }
+          ]
+        }
+      ]
+    }
+
+    const mockSiteResponse = { id: 1, 
+                               sites: [...mockRawSiteResponse["objects"] as unknown as Site[],
+                               ...mockRawSiteResponseDefault["objects"] as unknown as Site[]]  } as unknown as LayerObject;
     // @ts-ignore
-    axios.get.mockResolvedValueOnce({ data: mockRawSiteResponse});
+    axios.get.mockResolvedValue({ data: mockRawSiteResponseDefault})
+              .mockResolvedValueOnce({ data: mockRawSiteResponse});
+
     const response = await getSites({ id: 1, sites: [] } as unknown as LayerObject, 3, 1);
-    console.log("Running test 3");
+
     expect(axios.get).toHaveBeenCalledWith(`${BASE_URL}/site/?limit=3&offset=0&layer=1`, {headers: HEADERS});
-    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledTimes(2);
     expect(response).toEqual(mockSiteResponse);
   });
 });
