61e4bce33ebf38bca7852ef0c6d50e1adfbfaefb
Ishuah Kariuki <kariuki@ishuah.com>
2022-04-18 10:27:16 +0300

    UI updates

--- src/components/StepLabel/StepLabel.tsx
+++ src/components/StepLabel/StepLabel.tsx
@@ -1,23 +1,48 @@
+import { Box, Text } from 'grommet';
 import React from 'react';
+import { Checkmark } from 'grommet-icons';
 
 type StepLabelProps = {
   text: string
   step: number
+  completed: boolean
+  active?: boolean
 }
 
 function StepLabel(props: StepLabelProps) {
-  const { text, step } = props;
+  const {
+    text, step, completed, active,
+  } = props;
+  const color = active || completed ? '#7D4CDB' : '#9e9e9e';
   return (
-    <span style={{ display: 'flex' }}>
-      <span style={{ display: 'flex', paddingRight: '8px' }}>
-        <svg height="25px" width="25px" focusable="false" aria-hidden="true">
-          <circle fill="#1976d2" cx="12" cy="12" r="12" />
-          <text fill="white" style={{ fontSize: '0.75rem' }} x="12" y="16" textAnchor="middle">{step}</text>
-        </svg>
+    <Box style={{
+      paddingRight: 8, paddingLeft: 8, flex: 1, position: 'relative',
+    }}
+    >
+      { step > 1
+      && (
+        <Box style={{
+          flex: 1, position: 'absolute', top: 12, left: 'calc(-50% + 20px)', right: 'calc(50% + 20px)',
+        }}
+        >
+          <span style={{ borderTop: '1px solid #bdbdbd' }} />
+        </Box>
+      )}
+      <span style={{ flexDirection: 'column', alignItems: 'center', display: 'flex' }}>
+        <span style={{ display: 'flex' }}>
+          <svg height="25px" width="25px" focusable="false" aria-hidden="true">
+            <circle fill={color} cx="12" cy="12" r="12" />
+            { completed ? (<Checkmark color="white" size="6px" />) : (<text fill="white" style={{ fontSize: '0.75rem' }} x="12" y="16" textAnchor="middle">{step}</text>) }
+          </svg>
+        </span>
+        <Text size="small" weight={500} margin={{ top: 'small' }}>{text}</Text>
       </span>
-      <span>{text}</span>
-    </span>
+    </Box>
   );
 }
+
+StepLabel.defaultProps = {
+  active: false,
+};
 
 export default StepLabel;
--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,38 +1,79 @@
-import React from 'react';
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable react/no-array-index-key */
+import React, { useState } from 'react';
 import {
-  Grommet, Header, Heading, Main, Box, Text, Card, CardHeader, CardBody, Image,
+  Grommet, Header, Heading, Main, Box, Text, WorldMap, Grid, Button,
 } from 'grommet';
 import StepLabel from '../components/StepLabel/StepLabel';
 
+interface Workflow {
+  steps: []
+}
+
 function App() {
+  const steps = ['Select map type', 'Load your data', 'Refine', 'Visualize'];
+  const [currentStep, setCurrentStep] = useState(0);
+
+  function advanceToNext() {
+    setCurrentStep(currentStep + 1);
+  }
+
+  function revertToLast() {
+    setCurrentStep(currentStep - 1);
+  }
+
+  function reset() {
+    setCurrentStep(0);
+  }
+
   return (
     <Grommet>
       <Main fill="vertical" pad="none">
         <Header background="white" pad="xxsmall">
           <Heading level="3" margin={{ vertical: 'small', horizontal: 'small' }}>Batian</Heading>
         </Header>
-        <Box pad="xlarge" align="center">
-          <Text size="xlarge">What kind of map do you want to create?</Text>
-          <Box direction="row" gap="large" margin={{ vertical: 'medium', horizontal: 'xlarge' }} pad="medium">
-            <Card height="small" width="small" background="white">
-              <CardHeader pad="none">
-                <Image
-                  fit="cover"
-                  src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"
-                />
-              </CardHeader>
-              <CardBody pad="medium">Symbol Map</CardBody>
-            </Card>
-            <Card height="small" width="small" background="white">
-              <CardHeader>
-                <Image
-                  fit="cover"
-                  src="//v2.grommet.io/assets/Wilderpeople_Ricky.jpg"
-                />
-              </CardHeader>
-              <CardBody pad="medium">Choropleth Map</CardBody>
-            </Card>
-          </Box>
+        <Box pad="large" align="center">
+          <Grid
+            rows={['large']}
+            columns={['large', 'large']}
+            gap="small"
+            areas={[
+              { name: 'nav', start: [0, 0], end: [0, 0] },
+              { name: 'main', start: [1, 0], end: [1, 0] },
+            ]}
+          >
+            <Box gridArea="nav" background="white">
+              <Box direction="row" pad="medium">
+                {
+                  steps
+                    .map((step, i) => (
+                      <StepLabel
+                        key={i}
+                        active={i === currentStep}
+                        completed={i < currentStep}
+                        text={step}
+                        step={i + 1}
+                      />
+                    ))
+                }
+              </Box>
+              <Box pad="medium">
+                <Box height="large">
+                  <Text>Step details</Text>
+                </Box>
+                <Box direction="row" justify="between">
+                  <Button onClick={revertToLast} alignSelf="start" label="Back" />
+                  <Box direction="row">
+                    <Button onClick={reset} alignSelf="end" label="Cancel" margin={{ right: 'small' }} />
+                    <Button onClick={advanceToNext} alignSelf="end" primary label="Continue" />
+                  </Box>
+                </Box>
+              </Box>
+            </Box>
+            <Box gridArea="main" pad="large">
+              <WorldMap color="brand" />
+            </Box>
+          </Grid>
         </Box>
       </Main>
     </Grommet>
