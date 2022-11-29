43b80ce44d9464e48d3b829934dbbd54782475ab
ishuah <kariuki@ishuah.com>
2022-11-29 23:30:30 +0300

    some minor fixes

--- src/components/VisualizeStep.tsx
+++ src/components/VisualizeStep.tsx
@@ -1,6 +1,6 @@
 import {
   Box, Heading, TableBody, TableRow,
-  TableCell, Text, Select,
+  TableCell, Text, Select, Table,
 } from 'grommet';
 import React from 'react';
 import { useRecoilState } from 'recoil';
@@ -14,55 +14,57 @@ function VisualizeStep() {
     <Box height="large">
       <Box pad="medium">
         <Heading level={3}>Visualize</Heading>
-        <TableBody>
-          { appState.map.type === 'Symbol'
-            ? (
-              <>
+        <Table>
+          <TableBody>
+            { appState.map.type === 'Symbol'
+              ? (
+                <>
+                  <TableRow>
+                    <TableCell>
+                      <Text margin="small">Select symbol shape:</Text>
+                    </TableCell>
+                    <TableCell>
+                      <Select
+                        options={SYMBOLS}
+                        value={appState.symbolShape}
+                        onChange={({ option }) => setAppState({ ...appState, symbolShape: option })}
+                      />
+                    </TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell>
+                      <Text margin="small">Select color:</Text>
+                    </TableCell>
+                    <TableCell>
+                      <Select
+                        options={SYMBOL_COLORS}
+                        value={appState.symbolColorScheme}
+                        onChange={({ option }) => {
+                          setAppState({ ...appState, symbolColorScheme: option });
+                        }}
+                      />
+                    </TableCell>
+                  </TableRow>
+                </>
+              )
+              : (
                 <TableRow>
                   <TableCell>
-                    <Text margin="small">Select symbol shape:</Text>
+                    <Text margin="small">Select palette:</Text>
                   </TableCell>
                   <TableCell>
                     <Select
-                      options={SYMBOLS}
-                      value={appState.symbolShape}
-                      onChange={({ option }) => setAppState({ ...appState, symbolShape: option })}
-                    />
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell>
-                    <Text margin="small">Select color:</Text>
-                  </TableCell>
-                  <TableCell>
-                    <Select
-                      options={SYMBOL_COLORS}
-                      value={appState.symbolColorScheme}
+                      options={CHOROPLETH_COLORS}
+                      value={appState.choroplethColorScheme}
                       onChange={({ option }) => {
-                        setAppState({ ...appState, symbolColorScheme: option });
+                        setAppState({ ...appState, choroplethColorScheme: option });
                       }}
                     />
                   </TableCell>
                 </TableRow>
-              </>
-            )
-            : (
-              <TableRow>
-                <TableCell>
-                  <Text margin="small">Select palette:</Text>
-                </TableCell>
-                <TableCell>
-                  <Select
-                    options={CHOROPLETH_COLORS}
-                    value={appState.choroplethColorScheme}
-                    onChange={({ option }) => {
-                      setAppState({ ...appState, choroplethColorScheme: option });
-                    }}
-                  />
-                </TableCell>
-              </TableRow>
-            )}
-        </TableBody>
+              )}
+          </TableBody>
+        </Table>
       </Box>
     </Box>
   );
